22aecce2bbc47a302d171ad658a200fbfe174f4d
manshad <manshadchangampalli010@gmail.com>
2022-05-19 20:36:06 +0530

    #added: the loading effect is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { lazy, Suspense } from "react";
 import "./App.css";
+import Loading from "./components/loading/Loading";
 const Page = lazy(() => import("./Page"));
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<h1>Loading</h1>}>
+      <Suspense fallback={<Loading/>}>
         <Page />
       </Suspense>
     </div>
