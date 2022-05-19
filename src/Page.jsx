bd29eb9704d1cee237ab2bf7c4167ea013b5c78b
manshad <manshadchangampalli010@gmail.com>
2022-05-19 15:34:57 +0530

    #added: the card lis ting section's head portion is finished

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import BookReviwer from './components/BookReviewer/BookReviwer'
+import CardsSection from './components/CardsSeciton/CardsSection'
 import Hero from './components/Hero/Hero'
 
 const Page = () => {
@@ -8,6 +9,7 @@ const Page = () => {
         <Hero/>
         <div style={{width:"100%",height:"20px",background:"#0E7C83"}} ></div>
         <BookReviwer/>
+        <CardsSection/>
     </div>
   )
 }
---
+++ src/components/CardsSeciton/CardsSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './CardsSection.scss'
+
+const CardsSection = () => {
+  return (
+    <div className='cards-section'>
+        <div className="header">
+            <h1>Check out latest reviews</h1>
+            <p>
+                see more
+                <img src="images/icons/arrow.svg" alt="" />
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default CardsSection
